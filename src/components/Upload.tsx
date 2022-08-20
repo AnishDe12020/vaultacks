a9cba6e7a2b06eb3581e609011af60342376c6bb
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-08-20 16:14:26 +0530

    feat(upload): :sparkles: add file upload support

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,3 +1,4 @@
+import useLoading from "@/hooks/use-loading";
 import { useStorage } from "@/hooks/use-storage";
 import {
   Box,
@@ -14,20 +15,84 @@ import {
   Switch,
   useToast,
   Flex,
+  Text,
+  Spinner,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useDropzone, FileRejection } from "react-dropzone";
+import { FilePlus } from "react-feather";
 
 const Upload = () => {
-  const [text, setText] = useState<string>("");
+  const [data, setData] = useState<string>("");
   const [filename, setFilename] = useState<string>("");
   const [isPublic, setPublic] = useState<boolean>(false);
+  const {
+    isLoading: isFileReadLoading,
+    startLoading: startFileReadLoading,
+    stopLoading: stopFileReadLoading,
+  } = useLoading();
+  const {
+    isLoading: isUploadLoading,
+    startLoading: startUploadLoading,
+    stopLoading: stopUploadLoading,
+  } = useLoading();
 
   const { saveFile } = useStorage();
 
   const toast = useToast();
 
+  const onDropAccepted = useCallback(
+    (files: File[]) => {
+      startFileReadLoading();
+      const file = files[0];
+      const reader = new FileReader();
+      reader.onerror = () => {
+        toast({
+          title: "Error reading file",
+          description: "There was an error reading the selected file",
+          status: "error",
+        });
+        stopFileReadLoading();
+      };
+
+      reader.onload = () => {
+        setData(reader.result as string);
+        setFilename(file.name);
+        toast({
+          title: "Read file",
+          description: "File read successfully",
+          status: "success",
+        });
+        stopFileReadLoading();
+      };
+
+      reader.readAsArrayBuffer(file);
+    },
+    [toast, startFileReadLoading, stopFileReadLoading]
+  );
+
+  const onDropRejected = useCallback(
+    (fileRejections: FileRejection[]) => {
+      toast({
+        status: "error",
+        title: "File Drop Rejected",
+        description:
+          fileRejections.length > 1
+            ? "You can only select 1 file"
+            : "Unknown error",
+      });
+    },
+    [toast]
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    maxFiles: 1,
+    onDropRejected,
+    onDropAccepted,
+  });
+
   const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setText(e.target.value);
+    setData(e.target.value);
   };
 
   const handleFilenameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -39,12 +104,6 @@ const Upload = () => {
   };
 
   const handleUpload = async () => {
-    let data;
-
-    if (text) {
-      data = text;
-    }
-
     const url = await saveFile(filename, data, isPublic);
 
     toast({
@@ -72,28 +131,58 @@ const Upload = () => {
             </FormControl>
           </TabPanel>
           <TabPanel>
-            <p>TODO: File upload</p>
+            <Flex
+              direction="column"
+              experimental_spaceY={4}
+              justifyContent="center"
+              alignItems="center"
+              border="2px"
+              borderStyle="dashed"
+              borderColor="whiteAlpha.600"
+              p={4}
+              rounded="lg"
+              cursor="pointer"
+              _hover={{ borderColor: "whiteAlpha.500" }}
+              {...getRootProps()}
+            >
+              <input {...getInputProps()} />
+
+              <FilePlus />
+
+              {isFileReadLoading ? (
+                <Spinner />
+              ) : isDragActive ? (
+                <Text>Drop here</Text>
+              ) : (
+                <>
+                  <Text>
+                    Drag and drop the file here, or click to select files
+                  </Text>
+                  <Text>Uploading only 1 file is supported for now</Text>
+                </>
+              )}
+            </Flex>
           </TabPanel>
         </TabPanels>
-
-        <Flex experimental_spaceY={4} flexDir="column">
-          <FormControl isRequired>
-            <FormLabel>Filename</FormLabel>
-            <Input onChange={handleFilenameChange} />
-          </FormControl>
-
-          <FormControl>
-            <FormLabel>Is Public?</FormLabel>
-            <Switch
-              id="isPublic"
-              isChecked={isPublic}
-              onChange={handlePublicChange}
-            />
-          </FormControl>
-
-          <Button onClick={handleUpload}>Upload</Button>
-        </Flex>
       </Tabs>
+
+      <Flex experimental_spaceY={4} flexDir="column">
+        <FormControl isRequired>
+          <FormLabel>Filename</FormLabel>
+          <Input onChange={handleFilenameChange} value={filename} />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Is Public?</FormLabel>
+          <Switch
+            id="isPublic"
+            isChecked={isPublic}
+            onChange={handlePublicChange}
+          />
+        </FormControl>
+
+        <Button onClick={handleUpload}>Upload</Button>
+      </Flex>
     </Box>
   );
 };
